06153cdabd686262fba93a6e40a53e383fe29591
AnktSnghmeh01a <ankitsinghmehra@12345678@gmail.com>
2024-01-22 07:11:12 +0530

    created input in 2nd row update the header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,86 +1,85 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import { HashLink } from "react-router-hash-link";
-import { AiOutlineSearch, AiOutlineShoppingCart ,AiOutlineMenu } from "react-icons/ai";
+import {
+  AiOutlineSearch,
+  AiOutlineShoppingCart,
+  AiOutlineMenu,
+} from "react-icons/ai";
 
 const Header = () => {
- 
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const handleMenuClick = () => {
+    const allLinks = document.querySelector(".links-part");
 
-    const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const handleMenuClick = () => {
+    if (isMenuOpen) {
+      allLinks.style.position = "relative";
+    } else {
+      allLinks.style.position = "absolute";
+    }
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-         const allLinks = document.querySelector(".links-part");
-
-        
-         if(isMenuOpen){
-          allLinks.style.position = 'relative';
-        }
-         else{
-          allLinks.style.position = 'absolute';
-        }  
-        setIsMenuOpen(!isMenuOpen);
-        };
-
-    return (
+  return (
     <header className="header">
 
       <div className="upper-header">
-       
+
         <div className="logo-part">
           <h1>ShopIn</h1>
         </div>
 
-        <div className="middle-div-navbar-portion">
-          <select
-            name="all-items"
-            id="my-items-topdown"
-            className="top-down-navbar-options-all"
-          >
-            <option value="0" label="All" select="selected">
-              All
-            </option>
+        <div className="right-portion-upper-header">
+        
+          <div className="middle-div-navbar-portion">
+            <select
+              name="all-items"
+              id="my-items-topdown"
+              className="top-down-navbar-options-all"
+            >
+              <option value="0" label="All" select="selected">
+                All
+              </option>
 
-            <option value="1" label="All Categories"></option>
+              <option value="1" label="All Categories"></option>
 
-            <option value="2" label="Home"></option>
+              <option value="2" label="Home"></option>
 
-            <option value="2" label="Clothing"></option>
+              <option value="2" label="Clothing"></option>
 
-            <option value="2" label="Books"></option>
+              <option value="2" label="Books"></option>
 
-            <option value="4" label="Electronics"></option>
+              <option value="4" label="Electronics"></option>
+            </select>
 
-          </select>
+            <input
+              type="text"
+              placeholder="Search your products"
+              className="input-bar-top-amazon-in"
+            />
 
-          <input
-            type="text"
-            placeholder="Search your products"
-            className="input-bar-top-amazon-in"
-          />
-
-          <div className="search-icon-input-bar">
-            <AiOutlineSearch className="search-icon" />
+            <div className="search-icon-input-bar">
+              <AiOutlineSearch className="search-icon" />
+            </div>
           </div>
-        </div>
-
-        <div className="cart-portion">
+          <div className="cart-portion">
           <AiOutlineShoppingCart className="cart-icon" />
         </div>
+        </div>
+
+      
       </div>
 
-     <div className="lower-header">
+      <div className="lower-header">
+        <div className="links-part">
+          <HashLink to={"/home"}>Home</HashLink>
+          <HashLink to={"/cloths"}>Clothing</HashLink>
+          <HashLink to={"/books"}>Books</HashLink>
+          <HashLink to={"/electronics"}>Electronics</HashLink>
+        </div>
 
-       <div className="links-part">
-        <HashLink to={"/home"}>Home</HashLink>
-        <HashLink to={"/cloths"}>Clothing</HashLink>
-        <HashLink to={"/books"}>Books</HashLink>
-        <HashLink to={"/electronics"}>Electronics</HashLink>
-       </div>
-    
-
-       <div className="menu">
-      <AiOutlineMenu className="menu-icon" onClick={handleMenuClick} />
-    </div>
-
+        <div className="menu">
+          <AiOutlineMenu className="menu-icon" onClick={handleMenuClick} />
+        </div>
       </div>
     </header>
   );
